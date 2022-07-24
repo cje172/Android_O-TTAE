6370b51161a70bbc5deeb3ad5debe657da1360f7
Jieun Choi <cje172@naver.com>
2022-07-24 16:29:27 +0900

    Bottom Navigation 구현

---
+++ app/src/main/java/com/example/chapter5_5/GiftFragment.kt
@@ -0,0 +1,19 @@
+package com.example.chapter5_5
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class GiftFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gift, container, false)
+    }
+
+}
--- app/src/main/java/com/example/chapter5_5/MainActivity.kt
+++ app/src/main/java/com/example/chapter5_5/MainActivity.kt
@@ -2,14 +2,54 @@ package com.example.chapter5_5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBnv: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mainBnv = findViewById(R.id.main_bnv)
 
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, HomeFragment())
+            .commitAllowingStateLoss()
+        mainBnv.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.giftFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, GiftFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.tasteFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, TasteFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.myFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, MyFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
 
