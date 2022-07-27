7b2b270d691504be6771797a95255ed2192073ee
hye-on <ain0103@naver.com>
2022-07-27 16:23:51 +0900

    결과내용 DB연결 성공 (미완성)

--- app/src/main/java/com/example/chapter5_5/MyFragment.kt
+++ app/src/main/java/com/example/chapter5_5/MyFragment.kt
@@ -1,6 +1,9 @@
 package com.example.chapter5_5
 
 import android.os.Bundle
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,10 +17,22 @@ import androidx.viewpager2.widget.ViewPager2
 import java.util.*
 
 
+
+
 class MyFragment : Fragment() {
     lateinit var Santa: FrameLayout
+    lateinit var mainActivity: MainActivity
     private var GiftDatas = ArrayList<MyGiftList>()
     lateinit var MyGiftItemRv: RecyclerView
+    lateinit var dbManager: DBManager
+    lateinit var sqlitedb: SQLiteDatabase
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        // 2. Context를 액티비티로 형변환해서 할당
+        mainActivity = context as MainActivity
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,17 +41,39 @@ class MyFragment : Fragment() {
 
         val view: View = inflater.inflate(R.layout.fragment_my, container, false)
 
+        //db 읽어서 배열에 저장
+        dbManager = DBManager(mainActivity,"my",null,1)
+        sqlitedb =dbManager.readableDatabase
+        var cursor: Cursor
+        //cursor =sqlitedb.rawQuery("SELECT * FROM my WHERE user = 'qqq';",null)
+        cursor =sqlitedb.rawQuery("SELECT * FROM my ;",null)
+       // if (cursor.moveToNext()){
+            var temp1 =cursor.getString((cursor.getColumnIndex("friendName"))).toString()
+            var temp2=cursor.getString((cursor.getColumnIndex("itemName"))).toString()
+            var temp3 =cursor.getString((cursor.getColumnIndex("price"))).toString()
+
+       // }
+
 
         MyGiftItemRv=view.findViewById(R.id.my_gift_list_rv)
         //  Santa = view.findViewById(R.id.santa)
+//        GiftDatas.apply {
+//            add(MyGiftList("ooo님을 위한 패션 아이템", "wonder visitor 볼캡", "12,000", R.drawable.wonder_visitor_ball_cap))
+//            add(MyGiftList("111님을 위한 머그잔", "아이보리앤도트 머그잔", "8,400", R.drawable.list2_cup_img))
+//            add(MyGiftList("222님을 위한 패션아이템", "Copenhagen-bule 에코백", "9,800", R.drawable.list3_bag_img))
+//            add(MyGiftList("333님을 위한 스몰 력셔리", "드레스 퍼퓸 100ml", "8,950", R.drawable.list4_perfume_img))
+//            add(MyGiftList("444님을 위한 스몰 력셔리", "드레스 퍼퓸 100ml", "8,950", R.drawable.list4_perfume_img))
+//            add(MyGiftList("555님을 위한 스몰 력셔리", "드레스 퍼퓸 100ml", "8,950", R.drawable.list4_perfume_img))
+//            add(MyGiftList("666님을 위한 패션 아이템", "wonder visitor 볼캡", "12,000", R.drawable.wonder_visitor_ball_cap))
+//        }
         GiftDatas.apply {
-            add(MyGiftList("ooo님을 위한 패션 아이템", "wonder visitor 볼캡", "12,000", R.drawable.wonder_visitor_ball_cap))
-            add(MyGiftList("111님을 위한 머그잔", "아이보리앤도트 머그잔", "8,400", R.drawable.list2_cup_img))
-            add(MyGiftList("222님을 위한 패션아이템", "Copenhagen-bule 에코백", "9,800", R.drawable.list3_bag_img))
-            add(MyGiftList("333님을 위한 스몰 력셔리", "드레스 퍼퓸 100ml", "8,950", R.drawable.list4_perfume_img))
-            add(MyGiftList("444님을 위한 스몰 력셔리", "드레스 퍼퓸 100ml", "8,950", R.drawable.list4_perfume_img))
-            add(MyGiftList("555님을 위한 스몰 력셔리", "드레스 퍼퓸 100ml", "8,950", R.drawable.list4_perfume_img))
-            add(MyGiftList("666님을 위한 패션 아이템", "wonder visitor 볼캡", "12,000", R.drawable.wonder_visitor_ball_cap))
+            add(MyGiftList(temp1, temp2, temp3, R.drawable.wonder_visitor_ball_cap))
+//            add(MyGiftList("111님을 위한 머그잔", "아이보리앤도트 머그잔", "8,400", R.drawable.list2_cup_img))
+//            add(MyGiftList("222님을 위한 패션아이템", "Copenhagen-bule 에코백", "9,800", R.drawable.list3_bag_img))
+//            add(MyGiftList("333님을 위한 스몰 력셔리", "드레스 퍼퓸 100ml", "8,950", R.drawable.list4_perfume_img))
+//            add(MyGiftList("444님을 위한 스몰 력셔리", "드레스 퍼퓸 100ml", "8,950", R.drawable.list4_perfume_img))
+//            add(MyGiftList("555님을 위한 스몰 력셔리", "드레스 퍼퓸 100ml", "8,950", R.drawable.list4_perfume_img))
+//            add(MyGiftList("666님을 위한 패션 아이템", "wonder visitor 볼캡", "12,000", R.drawable.wonder_visitor_ball_cap))
         }
 
 
--- app/src/main/java/com/example/chapter5_5/ResultFragment.kt
+++ app/src/main/java/com/example/chapter5_5/ResultFragment.kt
@@ -1,52 +1,76 @@
 package com.example.chapter5_5
 
+
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 
 
 
 
 class ResultFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
+
+    lateinit var dbManager: DBManager
+    lateinit var sqlitedb: SQLiteDatabase
+    lateinit var mainActivity: MainActivity
+    lateinit var friendItem: TextView
+    lateinit var productName: TextView
+    lateinit var giftImg:ImageView
+    lateinit var price: TextView
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        // 2. Context를 액티비티로 형변환해서 할당
+        mainActivity = context as MainActivity
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+
+
+
+        val view: View = inflater.inflate(R.layout.fragment_result, container, false)
+        dbManager = DBManager(mainActivity,"myDB",null,1)
+        friendItem = view.findViewById(R.id.friendItem)
+        productName = view.findViewById(R.id. productName)
+        price = view.findViewById(R.id.price)
+        giftImg=view.findViewById(R.id.coverImg)
+
+       // var myId:String="qqq"
+        var friendItemTemp:String =friendItem.text.toString()
+        var productNameTemp:String =productName.text.toString()
+        var priceTemp:String =price.text.toString()
+
+        sqlitedb =dbManager.writableDatabase
+
+        sqlitedb.execSQL("INSERT INTO myDB VALUES('"+friendItemTemp+"','"+productNameTemp+"','"+priceTemp+"','"+priceTemp+"');")
+
+        sqlitedb.close()
+
+        giftImg.setImageResource(R.drawable.wonder_visitor_ball_cap)
+        return  view
     }
 
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment ResultFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            ResultFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
+
 }
